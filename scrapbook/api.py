499784989e5c5363816a6c894b81e674208ae60b
Matthew Seal <mseal007@gmail.com>
2019-05-07 21:07:30 -0700

    Fixed import name

--- scrapbook/api.py
+++ scrapbook/api.py
@@ -20,7 +20,7 @@ from .models import Notebook, Scrapbook
 from .scraps import Scrap, scrap_to_payload
 from .schemas import GLUE_PAYLOAD_FMT
 from .encoders import registry as encoder_registry
-from .utils import no_action_without_kernel
+from .utils import kernel_required
 
 
 @kernel_required
